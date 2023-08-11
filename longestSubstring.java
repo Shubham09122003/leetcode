6bc02785c373586ee8b0be9ef4207c6daabbf931
Shubham Chauhan <shubhamsc9504@gmail.com>
2023-08-11 10:29:30 +0530

    valid longet string

---
+++ longestSubstring.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+      Set<Character>st=new HashSet<>();
+       int max_size=0,left=0;
+       for(int right=0;right<s.length();right++){
+           if(!st.contains(s.charAt(right))){
+            st.add(s.charAt(right));
+             max_size=Math.max(max_size,right-left+1);
+           }
+           else{
+               if(st.size()>max_size){
+                  ;
+               }
+               while(st.contains(s.charAt(right))){
+                   st.remove(s.charAt(left));
+                   left++;
+               }
+               st.add(s.charAt(right));
+           }
+           
+       }
+       return max_size;
+    }
+}
