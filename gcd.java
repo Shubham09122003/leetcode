02f3df50a51e60ed57aefc00dfdb82b0c49d76e0
Shubham Chauhan <shubhamsc9504@gmail.com>
2023-09-26 08:26:37 +0530

    gcd

---
+++ gcd.java
@@ -0,0 +1,12 @@
+public class gcd {
+    public static void main(String[] args) {
+        int x=12;int y=8;int gcd=1;
+
+        for(int i=1;i<=x && i<=y;i++){
+            if(x%i==0 && y%i==0){
+                gcd=i;
+            }
+        }
+        System.out.print(gcd);
+        }
+}
